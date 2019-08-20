98c173f74d673bd5d72f160c35a6d728ac59eec6
Julian Rojas <jrojas@mcclnvl003.local>
2019-08-20 12:35:34 -0700

    idk what I'm doing lol
    Here we go!

--- index.js
+++ index.js
@@ -1,153 +1,128 @@
-﻿// Copyright (c) 2018 8th Wall, Inc.
-const imageFramePipelineModule = () => {
-  const LIGHT_PURPLE = 0xAD50FF
-  const CHERRY = 0xDD0065
-  const MINT = 0x00EDAF
-  const MANGO = 0xFFC828
-  const GRAY = 0x8083A2
-  const DARK_GRAY = 0x464766
+AFRAME.registerComponent('artgalleryframe', {
+  schema: {
+    name: {type: 'string'},
+    rotated: {type: 'bool'},
+    metadata: {type: 'string'},
+  },
+  init: function () {
+    const contents = document.getElementById('contents')
+    const container = document.getElementById('container')
 
-  const allFrames = {}
-  let scene
+    const {object3D, sceneEl} = this.el
 
-  // Renders one side of a picture frame rectangle.
-  const frameEdge = (x, y, w1, h1, w2, h2) => {
-    const edge = new THREE.Group()
-    const big = new THREE.Mesh(new THREE.CubeGeometry(w1, h1, 0.008),
-      new THREE.MeshBasicMaterial({color: DARK_GRAY}))
-    big.position.set(x, y, 0)
-    const small = new THREE.Mesh(new THREE.CubeGeometry(w2, h2, 0.012),
-      new THREE.MeshBasicMaterial({color: GRAY}))
-    small.position.set(x, y, 0)
-    edge.add(big)
-    edge.add(small)
-    return edge
-  }
+    // Hide the image target until it is found
+    object3D.visible = false
 
-  // Renders a picture frame rectangle.
-  const frameBorder = (scaledWidth, scaledHeight) => {
-    const border = new THREE.Group()
-    border.add(frameEdge(-scaledWidth / 2, 0, 0.05, scaledHeight + 0.05, 0.03, scaledHeight + 0.03))
-    border.add(frameEdge(scaledWidth / 2, 0, 0.05, scaledHeight + 0.05, 0.03, scaledHeight + 0.03))
-    border.add(frameEdge(0, -scaledHeight / 2, scaledWidth + 0.05, 0.05, scaledWidth + 0.03, 0.03))
-    border.add(frameEdge(0, scaledHeight / 2, scaledWidth + 0.05, 0.05, scaledWidth + 0.03, 0.03))
-    return border
-  }
+    // Metadata comes to the primitive as a string, so we parse and destructure it
+    const {artist, date, title, wikiTitle} = JSON.parse(this.data.metadata)
 
-  // Adds a tinted-glass effect.
-  const framePane = (scaledWidth, scaledHeight) => {
-    const material = new THREE.MeshBasicMaterial({color: LIGHT_PURPLE})
-    material.alphaMap = new THREE.DataTexture(new Uint8Array([0, 127, 0]), 1, 1, THREE.RGBFormat)
-    material.alphaMap.needsUpdate = true
-    material.transparent = true
-    return new THREE.Mesh(new THREE.CubeGeometry(scaledWidth, scaledHeight, 0.001), material)
-  }
-
-  // Adds an oriented axis.
-  const axis = () => {
-    const axes = new THREE.Group()
-    const axisLength = 0.2
-    const cylinder = new THREE.CylinderBufferGeometry(0.01, 0.01, axisLength, 32)
-    const xAxis = new THREE.Mesh(cylinder, new THREE.MeshBasicMaterial({color: MANGO}))
-    const yAxis = new THREE.Mesh(cylinder, new THREE.MeshBasicMaterial({color: CHERRY}))
-    const zAxis = new THREE.Mesh(cylinder, new THREE.MeshBasicMaterial({color: MINT}))
-    xAxis.rotateZ(Math.PI / 2)
-    xAxis.position.set(axisLength / 2, 0, 0)
-    yAxis.position.set(0, axisLength / 2, 0)
-    zAxis.rotateX(Math.PI / 2)
-    zAxis.position.set(0, 0, axisLength / 2)
-    axes.add(xAxis)
-    axes.add(yAxis)
-    axes.add(zAxis)
-    return axes
-  }
-
-  // Constructs a picture frame out of threejs primitives.
-  const buildPrimitiveFrame = ({scaledWidth, scaledHeight}) => {
-    const frame = new THREE.Group()
-    frame.add(frameBorder(scaledWidth, scaledHeight))
-    frame.add(framePane(scaledWidth, scaledHeight))
-    frame.add(axis())
-    scene.add(frame)
-    return frame
-  }
-
-  // Updates the position of a picture frame that covers an image target.
-  const showImageFrame = ({detail}) => {
-    let frame = allFrames[detail.name]
-    if (!frame) {
-      frame = buildPrimitiveFrame(detail)
-      allFrames[detail.name] = frame
+    const frameEl = document.createElement('a-entity')
+    frameEl.setAttribute('scale', '0.95 0.95 0.95')
+    frameEl.setAttribute('gltf-model', '#frame-model')
+    if (this.data.rotated) {
+      // Rotate the frame for a landscape target
+      frameEl.setAttribute('rotation', '0 0 90')
     }
-    frame.position.copy(detail.position)
-    frame.quaternion.copy(detail.rotation)
-    frame.scale.set(detail.scale, detail.scale, detail.scale)
-    frame.visible = true
-  }
+    this.el.appendChild(frameEl)
 
-  // Hides the image frame when the target is no longer detected.
-  const hideImageFrame = ({detail}) => {
-    allFrames[detail.name].visible = false
-  }
+    // Instantiate the element with information about the painting
+    const infoDisplay = document.createElement('a-entity')
+    infoDisplay.setAttribute('info-display', {title, artist, date})
+    infoDisplay.object3D.position.set(0, this.data.rotated ? -0.4 : -0.5, 0.1)
+    this.el.appendChild(infoDisplay)
 
-  // Grab a handle to the threejs scene and set the camera position on pipeline startup.
-  const onStart = ({canvasWidth, canvasHeight}) => {
-    // Get the 3js sceen from xr3js.
-    const {camera} = XR.Threejs.xrScene()
-    scene = XR.Threejs.xrScene().scene
-
-    // Set the initial camera position relative to the scene we just laid out. This must be at a
-    // height greater than y=0.
-    camera.position.set(0, 3, 0)
-
-    // Sync the xr controller's 6DoF position and camera paremeters with our scene.
-    XR.XrController.updateCameraProjectionMatrix({
-      origin: camera.position,
-      facing: camera.quaternion,
+    // Use the title of the painting to fetch some info from the Wikipedia API
+    // If a painting doesn't have a Wikipedia article of its own, we use the painter's article via wikiTitle
+    const apiUrl = `https://en.wikipedia.org/w/api.php?action=query&titles=${wikiTitle || title}&format=json&prop=extracts&exintro=1&origin=*`
+    let pageContent
+    fetch(apiUrl, {mode: 'cors'})
+    .then(e => e.json())
+    .then(data => {
+      const page = Object.entries(data.query.pages)[0][1]
+      pageContent = `<h1>${page.title}</h1>${page.extract}`
     })
+
+    const tapTarget = document.createElement('a-box')
+    // Image targets are 3:4 so the target is scaled to match
+    tapTarget.setAttribute('scale', '0.75 1 0.1')
+    tapTarget.setAttribute('material', 'opacity: 0; transparent:true')
+    if (this.data.rotated) {
+      // Rotate the tap target for a landscape target
+      tapTarget.setAttribute('rotation', '0 0 90')
+    }
+    this.el.appendChild(tapTarget)
+
+    tapTarget.addEventListener('click', e => {
+      // Set the innerHTML of our UI element to the data returned by the API
+      contents.innerHTML = pageContent
+      // Removing the collapsed class from container triggers a CSS transition to show the content
+      container.classList.remove('collapsed')
+    })
+
+    // showImage handles displaying and moving the virtual object to match the image
+    const showImage = ({detail}) => {
+      // Updating position/rotation/scale using object3D is more performant than setAttribute
+      object3D.position.copy(detail.position)
+      object3D.quaternion.copy(detail.rotation)
+      object3D.scale.set(detail.scale, detail.scale, detail.scale)
+      object3D.visible = true
+      // Add tapTarget as a clickable object
+      tapTarget.classList.add('cantap')
+    }
+
+    // hideImage handles hiding the virtual object when the image target is lost
+    const hideImage = () => {
+      object3D.visible = false
+      // Remove tapTarget from clickable objects
+      tapTarget.classList.remove('cantap')
+    }
+
+    // These events are routed and dispatched by xrextras-generate-image-targets
+    this.el.addEventListener('xrimagefound', showImage)
+    this.el.addEventListener('xrimageupdated', showImage)
+    this.el.addEventListener('xrimagelost', hideImage)
   }
+})
 
-  return {
-    // Camera pipeline modules need a name. It can be whatever you want but must be unique within
-    // your app.
-    name: 'targetframes',
+// This component uses the A-Frame text component to display information about a painting
+AFRAME.registerComponent('info-display', {
+  schema: {
+    title: {default: ''},
+    artist: {default: ''},
+    date: {default: ''},
+  },
+  init: function() {
+    // Limit title to 20 characters
+    const displayTitle = this.data.title.length > 20 ? `${this.data.title.substring(0, 17)}...` : this.data.title
+    const text = displayTitle + '\n' + this.data.artist + ', ' + this.data.date
+    const textData = {
+      align: 'left',
+      width: 0.7,
+      wrapCount: 22,
+      value: text,
+      color: 'white',
+    }
 
-    // onStart is called once when the camera feed begins. In this case, we need to wait for the
-    // XR.Threejs scene to be ready before we can access it to add content. It was created in
-    // XR.Threejs.pipelineModule()'s onStart method.
-    onStart,
+    this.el.setAttribute('text', textData )
 
-    // Listeners are called right after the processing stage that fired them. This guarantees that
-    // updates can be applied at an appropriate synchronized point in the rendering cycle.
-    listeners: [
-      {event: 'reality.imagefound', process: showImageFrame},
-      {event: 'reality.imageupdated', process: showImageFrame},
-      {event: 'reality.imagelost', process: hideImageFrame},
-    ],
+    // Instantiate a second text object behind the first to achieve an shadow effect
+    const textShadowEl = document.createElement('a-entity')
+    textData.color = 'black'
+    textShadowEl.setAttribute('text', textData)
+    textShadowEl.object3D.position.z = -0.01
+    this.el.appendChild(textShadowEl)
   }
-}
+})
 
-const onxrloaded = () => {
-  // If your app only interacts with image targets and not the world, disabling world tracking can
-  // improve speed.
-  XR.xrController().configure({disableWorldTracking: true})
-  XR.addCameraPipelineModules([  // Add camera pipeline modules.
-    // Existing pipeline modules.
-    XR.GlTextureRenderer.pipelineModule(),       // Draws the camera feed.
-    XR.Threejs.pipelineModule(),                 // Creates a ThreeJS AR Scene.
-    XR.XrController.pipelineModule(),            // Enables SLAM tracking.
-    XRExtras.AlmostThere.pipelineModule(),       // Detects unsupported browsers and gives hints.
-    XRExtras.FullWindowCanvas.pipelineModule(),  // Modifies the canvas to fill the window.
-    XRExtras.Loading.pipelineModule(),           // Manages the loading screen on startup.
-    XRExtras.RuntimeError.pipelineModule(),      // Shows an error image on runtime error.
-    // Custom pipeline modules.
-    imageFramePipelineModule(),                  // Draws a frame around detected image targets.
-  ])
+// xrextras-generate-image-targets uses this primitive to automatically populate multiple image targets
+AFRAME.registerPrimitive('artgallery-frame', {
+  defaultComponents: {
+    artgalleryframe: {},
+  },
 
-  // Open the camera and start running the camera run loop.
-  XR.run({canvas: document.getElementById('camerafeed')})
-}
-
-// Show loading screen before the full XR library has been loaded.
-const load = () => { XRExtras.Loading.showLoading({onxrloaded}) }
-window.onload = () => { window.XRExtras ? load() : window.addEventListener('xrextrasloaded', load) }
+  mappings: {
+    name: 'artgalleryframe.name',
+    rotated: 'artgalleryframe.rotated',
+    metadata: 'artgalleryframe.metadata',
+  }
+})
